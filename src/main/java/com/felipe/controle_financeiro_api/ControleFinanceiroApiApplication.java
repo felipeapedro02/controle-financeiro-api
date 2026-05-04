58cb259bd5f714c8a5eaabb875408739ee925002
Felipe Alves Pedro <felipealvespedro@gmail.com>
2026-05-04 15:03:49 -0300

    Cria estrutura inicial do projeto Spring

---
+++ src/main/java/com/felipe/controle_financeiro_api/ControleFinanceiroApiApplication.java
@@ -0,0 +1,13 @@
+package com.felipe.controle_financeiro_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleFinanceiroApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleFinanceiroApiApplication.class, args);
+	}
+
+}
